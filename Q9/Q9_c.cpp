7a091320b571975fe1d6820ea9d3495a9c340c59
Mohammed Ismail <mohammedismailck123@gmail.com>
2022-03-24 18:44:00 +0530

    Create Q9_c.cpp

---
+++ Q9/Q9_c.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <GL/freeglut.h>
+#include<cmath>
+using namespace std;
+
+void renderFunction()
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glLineWidth(2.0);
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    for(double theta = 0.0, radius = 0.7; theta < 2*M_PI; theta += 0.001){
+        glBegin(GL_LINE_STRIP);
+            glVertex2f(0.0, 0.0);
+            glVertex2f(radius*cos(theta), radius*sin(theta));
+        glEnd();
+    }
+
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINE_LOOP);
+    for(double theta = 0.0, radius = 0.7; theta <= 2*M_PI; theta += 0.0001){
+            glVertex2f(radius*cos(theta), radius*sin(theta));
+    }
+    glEnd();
+    glFlush();
+}
+
+void changeInteriorToGreen(int button, int state, int x, int y){
+    if(button == GLUT_LEFT_BUTTON){
+        glColor3f(0.0, 1.0, 0.0);
+        for(double theta = 0.0, radius = 0.7; theta < 2*M_PI; theta += 0.001){
+            glBegin(GL_LINE_STRIP);
+                glVertex2f(0.0, 0.0);
+                glVertex2f(radius*cos(theta), radius*sin(theta));
+            glEnd();
+        }
+        glFlush();
+    }
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(700, 700);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Q9_B - Window");
+    glutMouseFunc(changeInteriorToGreen);
+    glutDisplayFunc(renderFunction);
+    glutMainLoop();
+    return 0;
+}
