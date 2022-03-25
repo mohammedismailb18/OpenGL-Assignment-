c0286c6ad49a78b6f12dc8a23a7dc4010966faa3
Mohammed Ismail <mohammedismailck123@gmail.com>
2022-03-25 16:49:14 +0530

    Update Q9_a.cpp

--- Q9/Q9_a.cpp
+++ Q9/Q9_a.cpp
@@ -9,12 +9,12 @@ void renderFunction()
     glColor3f(1.0, 1.0, 1.0);
     glLineWidth(2.0);
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-    for(double theta = 0.0, radius = 0.7; theta < 2*M_PI; theta += 0.001){
-        glBegin(GL_LINE_STRIP);
-            glVertex2f(0.0, 0.0);
-            glVertex2f(radius*cos(theta), radius*sin(theta));
-        glEnd();
-    }
+    int numberOfVertices = 1000;
+    float radius = 0.7;
+    glBegin(GL_TRIANGLE_FAN);
+    for(int i=0; i<numberOfVertices; i++)
+        glVertex2f(radius*cos(((float)i/numberOfVertices)*2*M_PI), radius*sin(((float)i/numberOfVertices)*2*M_PI));
+    glEnd();
     glFlush();
 }
 
@@ -22,9 +22,9 @@ int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(1000, 1000);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Q9_a - Window");
+    glutInitWindowSize(700, 700);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Q9_A - Window");
     glutDisplayFunc(renderFunction);
     glutMainLoop();
     return 0;
